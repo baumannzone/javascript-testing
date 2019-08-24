d19b03226a9616cc8a8a9a4d5359f6c5d2e5948b
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-08-24 18:32:56 +0200

    Crear funciones

---
+++ funciones.js
@@ -0,0 +1,17 @@
+/**
+ * Suma 2 números y retorna su valor
+ * @param num1 {number}
+ * @param num2 {number}
+ * @returns {*}
+ */
+const suma = ( num1, num2 ) => num1 + num2
+
+/**
+ * Resta 2 números y retorna su valor
+ * @param num1
+ * @param num2
+ * @returns {number}
+ */
+const resta = ( num1, num2 ) => num1 - num2
+
+module.exports = { suma, resta }
