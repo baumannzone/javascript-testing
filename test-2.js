283b7298381d809b5357e6f2d2556b7823472996
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-08-24 19:28:33 +0200

    Crear test 2

---
+++ test-2.js
@@ -0,0 +1,24 @@
+/**
+ * test-2.js
+ * https://nodejs.org/api/assert.html#assert_assert
+ */
+const assert = require( 'assert' )
+const { suma, resta } = require( './funciones' )
+
+let resultado, esperado
+
+// Probar suma (2+3)
+resultado = suma( 2, 3 )
+esperado = 5
+if ( resultado !== esperado ) {
+  throw new Error( `${ resultado } es distinto de ${ esperado }` )
+}
+
+// Probar resta (8-3)
+resultado = resta( 8, 3 )
+esperado = 5
+if ( resultado !== esperado ) {
+  throw new Error( `${ resultado } es distinto de ${ esperado }` )
+}
+
+console.log( '¡Test1 OK! 👌' )
